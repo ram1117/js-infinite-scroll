9d1db5feebe821cd15a290733621b16337dcc0d3
ram1117 <ramkumark1@outlook.com>
2023-07-19 21:05:54 +0530

    finish infinite scroll loading, complete with loading spinner

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let booksBaseUrl = 'http://gutendex.com/books/?page=';
 let page = 1;
 const booksContainerEl = document.getElementById('books-container');
+const loader = document.getElementById('loader');
 
 const fetchBooks = async (page) => {
   const response = await fetch(`${booksBaseUrl}${page}`);
@@ -11,8 +12,12 @@ const fetchBooks = async (page) => {
   return data.results;
 };
 
-const displayBooks = async () => {
-  results = await fetchBooks(page);
+const displayBooks = async (pageNo) => {
+  loader.classList.toggle('invisible'); 
+  results = await fetchBooks(pageNo);
+  const pageInfo = document.createElement('h5');
+  pageInfo.textContent = `Page - ${pageNo}`;
+  booksContainerEl.appendChild(pageInfo);
   results.forEach((book) => {
     const item = document.createElement('div');
     item.classList.add('book-item');
@@ -27,14 +32,26 @@ const displayBooks = async () => {
     </div>`;
     booksContainerEl.appendChild(item);
   });
+  loader.classList.toggle('invisible');
 };
 
-const joinAuthors = (authors) =>{
-  let auths = "";
-  authors.forEach(author=>{
-    auths+=` ${author.name}`
-  })
+const joinAuthors = (authors) => {
+  let auths = '';
+  authors.forEach((author) => {
+    auths += ` ${author.name}`;
+  });
   return auths;
-}
+};
 
-displayBooks();
+window.addEventListener(
+  'scroll',
+  () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      page++;
+      displayBooks(page);
+    }
+  },
+  { passive: true }
+);
+displayBooks(page);
